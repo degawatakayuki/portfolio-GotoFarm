691c906441442771fd27b07ac6f0404420378643
degawatakayuki <>
2024-03-21 10:35:12 +0900

    swiper実装

--- js/bamburger.js
+++ js/bamburger.js
@@ -1,17 +1,25 @@
-// let menu_btn = document.getElementById('hamburger-btn');
-// let header_nav = document.getElementById('headerNavi');
-// let farstViewBg = document.getElementById('farstView');
-
-// menu_btn.addEventListener('click' , function () {
-//     header_nav.classList.toggle('menu-open');
-//     farstViewBg.classList.toggle('menu-open');
-// } );
-
-let menu_btn = document.getElementById('navToggle');
-let header_nav = document.getElementById('headerNavi');
-let farstViewBg = document.getElementById('farstView');
-
+let menu_btn = document.getElementById('hambugerIcon');
+//ハンバーガーアイコンをクリックした時の動作
 menu_btn.addEventListener('click' , function () {
-    // header_nav.classList.toggle('menu-open');
-    farstViewBg.classList.toggle('menu-open');
+    
+    //ハンバーガーアイコンに「open」クラスがついているか判定
+    if($(this).hasClass('c-hambugerIcon--open')) {
+        $(this).toggleClass('c-hambugerIcon--open');
+        $(".c-hambugerIcon__line").removeClass('c-hambugerIcon__line--open');
+        $(".mv__cover").removeClass('mv__cover--open');
+        $(".mv__navSp").css("display", "none");
+        
+    } else {
+        $(this).toggleClass('c-hambugerIcon--open');
+        $(".c-hambugerIcon__line").addClass('c-hambugerIcon__line--open');
+        $(".mv__cover").addClass('mv__cover--open');
+        $(".mv__navSp").slideDown();
+    }
+    
 } );
+$(".mv__link").on('click' , (e) => {
+    $(".mv__cover").removeClass('mv__cover--open');
+    $(".c-hambugerIcon").removeClass('c-hambugerIcon--open');
+    $(".c-hambugerIcon__line").removeClass('c-hambugerIcon__line--open');
+    $(".mv__navSp").css("display", "none");
+});
